0dc1d3f2c286ee6b9b7eb82eabe13cc7cc94b429
Timo Kramer <git@timokramer.de>
2025-07-01 10:34:40 +0200

    add new options to help text

--- project/src/main.c
+++ project/src/main.c
@@ -11,6 +11,8 @@ void print_usage(char *argv[]) {
     printf("Usage: %s -n -f <database file>\n", argv[0]);
     printf("\t -n create new database file\n");
     printf("\t -f (required) path to database file\n");
+    printf("\t -a add entry to database\n");
+    printf("\t -l list entries of database\n");
     return;
 }
 
